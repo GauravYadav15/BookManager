4344e9132bad58ffb6f93f123e7f65e643b18184
Gaurav Singh Yadav <93148442+GauravYadav15@users.noreply.github.com>
2023-05-12 21:58:16 +0530

    repo switched

---
+++ client/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {
+      // URL: "http://localhost:5000",
+      URL: "https://bookmanager.onrender.com"
+
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
